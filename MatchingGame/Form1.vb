a81be8339d322d6ad726f979705cc00a96819ca5
Tamara <tami.pg94@gmail.com>
2021-10-20 17:44:40 +0200

    commit

--- MatchingGame/Form1.vb
+++ MatchingGame/Form1.vb
@@ -1,3 +1,11 @@
 ﻿Public Class Form1
 
+    ' Utilice este objeto aleatorio para elegir iconos aleatorios para los cuadrados.
+    Dim random As New Random
+
+    ' Cada una de estas letras es un icono interesante en la fuente Webdings,
+    ' y cada icono aparece dos veces en esta lista.
+    Dim temporaryArray() As String = {"!", "!", "N", "N", ",", ",", "k", "k",
+                                      "b", "b", "v", "v", "w", "w", "z", "z"}
+    Dim icons As List(Of String) = temporaryArray.ToList()
 End Class
