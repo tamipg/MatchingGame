af8ba9edaab61d0851fcb3befe57de18d0bc0d4c
Tamara <tami.pg94@gmail.com>
2021-10-21 02:09:31 +0200

    commit

--- MatchingGame/Form1.Designer.vb
+++ MatchingGame/Form1.Designer.vb
@@ -22,23 +22,25 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.Label5 = New System.Windows.Forms.Label()
-        Me.Label6 = New System.Windows.Forms.Label()
-        Me.Label7 = New System.Windows.Forms.Label()
-        Me.Label8 = New System.Windows.Forms.Label()
-        Me.Label9 = New System.Windows.Forms.Label()
-        Me.Label10 = New System.Windows.Forms.Label()
-        Me.Label11 = New System.Windows.Forms.Label()
-        Me.Label12 = New System.Windows.Forms.Label()
-        Me.Label13 = New System.Windows.Forms.Label()
-        Me.Label14 = New System.Windows.Forms.Label()
-        Me.Label15 = New System.Windows.Forms.Label()
         Me.Label16 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.TableLayoutPanel1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -77,159 +79,16 @@ Partial Class Form1
         Me.TableLayoutPanel1.Size = New System.Drawing.Size(534, 511)
         Me.TableLayoutPanel1.TabIndex = 0
         '
-        'Label1
+        'Label16
         '
-        Me.Label1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Label1.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
-        Me.Label1.Location = New System.Drawing.Point(3, 0)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(127, 127)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "c"
-        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'Label2
-        '
-        Me.Label2.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Label2.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
-        Me.Label2.Location = New System.Drawing.Point(136, 0)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(127, 127)
-        Me.Label2.TabIndex = 1
-        Me.Label2.Text = "c"
-        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'Label3
-        '
-        Me.Label3.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Label3.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
-        Me.Label3.Location = New System.Drawing.Point(269, 0)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(127, 127)
-        Me.Label3.TabIndex = 2
-        Me.Label3.Text = "c"
-        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'Label4
-        '
-        Me.Label4.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Label4.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
-        Me.Label4.Location = New System.Drawing.Point(402, 0)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(129, 127)
-        Me.Label4.TabIndex = 3
-        Me.Label4.Text = "c"
-        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'Label5
-        '
-        Me.Label5.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Label5.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
-        Me.Label5.Location = New System.Drawing.Point(3, 127)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(127, 127)
-        Me.Label5.TabIndex = 4
-        Me.Label5.Text = "c"
-        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'Label6
-        '
-        Me.Label6.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Label6.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
-        Me.Label6.Location = New System.Drawing.Point(136, 127)
-        Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(127, 127)
-        Me.Label6.TabIndex = 5
-        Me.Label6.Text = "c"
-        Me.Label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'Label7
-        '
-        Me.Label7.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Label7.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
-        Me.Label7.Location = New System.Drawing.Point(269, 127)
-        Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(127, 127)
-        Me.Label7.TabIndex = 6
-        Me.Label7.Text = "c"
-        Me.Label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'Label8
-        '
-        Me.Label8.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Label8.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
-        Me.Label8.Location = New System.Drawing.Point(402, 127)
-        Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(129, 127)
-        Me.Label8.TabIndex = 7
-        Me.Label8.Text = "c"
-        Me.Label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'Label9
-        '
-        Me.Label9.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Label9.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
-        Me.Label9.Location = New System.Drawing.Point(3, 254)
-        Me.Label9.Name = "Label9"
-        Me.Label9.Size = New System.Drawing.Size(127, 127)
-        Me.Label9.TabIndex = 8
-        Me.Label9.Text = "c"
-        Me.Label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'Label10
-        '
-        Me.Label10.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Label10.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
-        Me.Label10.Location = New System.Drawing.Point(136, 254)
-        Me.Label10.Name = "Label10"
-        Me.Label10.Size = New System.Drawing.Size(127, 127)
-        Me.Label10.TabIndex = 9
-        Me.Label10.Text = "c"
-        Me.Label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'Label11
-        '
-        Me.Label11.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Label11.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
-        Me.Label11.Location = New System.Drawing.Point(269, 254)
-        Me.Label11.Name = "Label11"
-        Me.Label11.Size = New System.Drawing.Size(127, 127)
-        Me.Label11.TabIndex = 10
-        Me.Label11.Text = "c"
-        Me.Label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'Label12
-        '
-        Me.Label12.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Label12.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
-        Me.Label12.Location = New System.Drawing.Point(402, 254)
-        Me.Label12.Name = "Label12"
-        Me.Label12.Size = New System.Drawing.Size(129, 127)
-        Me.Label12.TabIndex = 11
-        Me.Label12.Text = "c"
-        Me.Label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'Label13
-        '
-        Me.Label13.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Label13.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
-        Me.Label13.Location = New System.Drawing.Point(3, 381)
-        Me.Label13.Name = "Label13"
-        Me.Label13.Size = New System.Drawing.Size(127, 130)
-        Me.Label13.TabIndex = 12
-        Me.Label13.Text = "c"
-        Me.Label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'Label14
-        '
-        Me.Label14.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Label14.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
-        Me.Label14.Location = New System.Drawing.Point(136, 381)
-        Me.Label14.Name = "Label14"
-        Me.Label14.Size = New System.Drawing.Size(127, 130)
-        Me.Label14.TabIndex = 13
-        Me.Label14.Text = "c"
-        Me.Label14.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.Label16.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Label16.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.Label16.Location = New System.Drawing.Point(402, 381)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(129, 130)
+        Me.Label16.TabIndex = 15
+        Me.Label16.Text = "c"
+        Me.Label16.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'Label15
         '
@@ -242,16 +101,163 @@ Partial Class Form1
         Me.Label15.Text = "c"
         Me.Label15.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
-        'Label16
+        'Label14
         '
-        Me.Label16.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Label16.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
-        Me.Label16.Location = New System.Drawing.Point(402, 381)
-        Me.Label16.Name = "Label16"
-        Me.Label16.Size = New System.Drawing.Size(129, 130)
-        Me.Label16.TabIndex = 15
-        Me.Label16.Text = "c"
-        Me.Label16.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.Label14.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Label14.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.Label14.Location = New System.Drawing.Point(136, 381)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(127, 130)
+        Me.Label14.TabIndex = 13
+        Me.Label14.Text = "c"
+        Me.Label14.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label13
+        '
+        Me.Label13.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Label13.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.Label13.Location = New System.Drawing.Point(3, 381)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(127, 130)
+        Me.Label13.TabIndex = 12
+        Me.Label13.Text = "c"
+        Me.Label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label12
+        '
+        Me.Label12.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Label12.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.Label12.Location = New System.Drawing.Point(402, 254)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(129, 127)
+        Me.Label12.TabIndex = 11
+        Me.Label12.Text = "c"
+        Me.Label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label11
+        '
+        Me.Label11.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Label11.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.Label11.Location = New System.Drawing.Point(269, 254)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(127, 127)
+        Me.Label11.TabIndex = 10
+        Me.Label11.Text = "c"
+        Me.Label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label10
+        '
+        Me.Label10.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Label10.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.Label10.Location = New System.Drawing.Point(136, 254)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(127, 127)
+        Me.Label10.TabIndex = 9
+        Me.Label10.Text = "c"
+        Me.Label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label9
+        '
+        Me.Label9.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Label9.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.Label9.Location = New System.Drawing.Point(3, 254)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(127, 127)
+        Me.Label9.TabIndex = 8
+        Me.Label9.Text = "c"
+        Me.Label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label8
+        '
+        Me.Label8.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Label8.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.Label8.Location = New System.Drawing.Point(402, 127)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(129, 127)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "c"
+        Me.Label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label7
+        '
+        Me.Label7.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Label7.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.Label7.Location = New System.Drawing.Point(269, 127)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(127, 127)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "c"
+        Me.Label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label6
+        '
+        Me.Label6.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Label6.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.Label6.Location = New System.Drawing.Point(136, 127)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(127, 127)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "c"
+        Me.Label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label5
+        '
+        Me.Label5.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Label5.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.Label5.Location = New System.Drawing.Point(3, 127)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(127, 127)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "c"
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label4
+        '
+        Me.Label4.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Label4.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.Label4.Location = New System.Drawing.Point(402, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(129, 127)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "c"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label3
+        '
+        Me.Label3.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Label3.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.Label3.Location = New System.Drawing.Point(269, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(127, 127)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "c"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Label2.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.Label2.Location = New System.Drawing.Point(136, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(127, 127)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "c"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label1
+        '
+        Me.Label1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Label1.Font = New System.Drawing.Font("Webdings", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.Label1.Location = New System.Drawing.Point(3, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(127, 127)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "c"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 750
         '
         'Form1
         '
@@ -283,4 +289,5 @@ Partial Class Form1
     Friend WithEvents Label3 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents Label1 As Label
+    Friend WithEvents Timer1 As Timer
 End Class
--- MatchingGame/Form1.vb
+++ MatchingGame/Form1.vb
@@ -9,10 +9,19 @@
                                       "b", "b", "v", "v", "w", "w", "z", "z"}
     Dim icons As List(Of String) = temporaryArray.ToList()
 
+    ' firstClicked apunta al primer control de etiqueta
+    ' que el jugador hace clic, pero no será nada
+    ' si el jugador aún no ha hecho clic en una etiqueta
+    Dim firstClicked As Label = Nothing
+
+    ' secondClicked apunta al segundo control Label que el jugador hace clic
+    Dim secondClicked As Label = Nothing
+
     Public Sub New()
         ' Esta llamada es requerida por el Diseñador de formularios de Windows
         InitializeComponent()
         ' Agregue cualquier inicialización después de la llamada InitializeComponent ()
+        AssignIconsToSquares()
     End Sub
 
     ''' <summary>
@@ -30,10 +39,90 @@
             If iconLabel IsNot Nothing Then
                 Dim randomNumber As Integer = random.Next(icons.Count)
                 iconLabel.Text = icons.ElementAt(randomNumber)
-                ' iconLabel.ForeColor = iconLabel.BackColor
+                iconLabel.ForeColor = iconLabel.BackColor
                 icons.RemoveAt(randomNumber)
             End If
         Next
 
+    End Sub
+
+    ''' <summary>
+    ''' El evento Click de cada etiqueta es manejado por este controlador de eventos
+    ''' </summary>
+    ''' <param name="sender">La etiqueta en la que se hizo click</param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub label_Click(sender As Object, e As EventArgs) Handles Label9.Click, Label8.Click, Label7.Click, Label6.Click, Label5.Click, Label4.Click, Label3.Click, Label2.Click, Label16.Click, Label15.Click, Label14.Click, Label13.Click, Label12.Click, Label11.Click, Label10.Click, Label1.Click
+        ' El temporizador solo se enciende después de 
+        ' mostrar dos iconos al jugador,
+        ' así que ignora cualquier click si el temporizador está funcionando
+        If (Timer1.Enabled = True) Then
+            Return
+        End If
+
+        Dim clickedLabel As Label = TryCast(sender, Label)
+
+        If clickedLabel IsNot Nothing Then
+            'Si la etiqueta en la que se hizo clic es negra, el jugador hizo clic
+            'un icono que ya ha sido revelado -
+            'ignora el clic
+            If (clickedLabel.ForeColor = Color.Black) Then
+                Return
+            End If
+            ' Si firstClicked es Nothing, este es el primer icono
+            ' en el par en el que el jugador hizo clic,
+            ' establece firstClicked en la etiqueta que el jugador
+            ' hace clic, cambia su color a negro y vuelve
+            If (firstClicked Is Nothing) Then
+                firstClicked = clickedLabel
+                firstClicked.ForeColor = Color.Black
+                Return
+            End If
+
+            ' Si firstClicked es Nothing, este es el primer icono
+            ' en el par en el que el jugador hizo clic,
+            ' así que establezca firstClicked clic en la etiqueta que el jugador
+            ' hace clic, cambia su color a negro y vuelve
+            If (firstClicked Is Nothing) Then
+                firstClicked = clickedLabel
+                firstClicked.ForeColor = Color.Black
+                Return
+            End If
+
+            ' Si el jugador llega tan lejos, el temporizador no
+            ' corriendo y firstClicked no es nada,
+            ' por lo que este debe ser el segundo icono en el que el jugador hizo clic
+            ' Establezca su color en negro
+            secondClicked = clickedLabel
+            secondClicked.ForeColor = Color.Black
+
+            ' Si el jugador llega tan lejos, el jugador
+            ' hizo clic en dos iconos diferentes, así que inicie el
+            ' timer (que esperará tres cuartos de
+            ' un segundo, y luego esconde los íconos)
+            Timer1.Start()
+        End If
+    End Sub
+
+    ''' <summary>
+    '' 'Este temporizador se inicia cuando el jugador hace clic
+    '' 'en dos iconos que no coinciden,
+    '' 'por lo que cuenta tres cuartos de segundo
+    '' 'y luego se apaga y oculta ambos íconos
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        ' detener el temporizador
+        Timer1.Stop()
+
+        ' Ocultar ambos iconos
+        firstClicked.ForeColor = firstClicked.BackColor
+        secondClicked.ForeColor = secondClicked.BackColor
+
+        ' Restablecer firstClicked y secondClicked
+        ' así la próxima vez que se haga click en una etiqueta
+        ' el programa sabe que es el primer click
+        firstClicked = Nothing
+        secondClicked = Nothing
     End Sub
 End Class
